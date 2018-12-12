607caeb98f3dc3fb5a19cd094650afa21b91a091
VGreg62 <43751188+VGreg62@users.noreply.github.com>
2018-12-12 09:36:44 +0100

    Ajout packages et classe Compte

---
+++ TPCompteBancaireJavaSE/src/main/java/fr/epsi/partie11/Compte.java
@@ -0,0 +1,20 @@
+package fr.epsi.partie11;
+
+public class Compte {
+	
+	public int solde;
+	
+	public void depotDe() {
+		
+	}
+	
+	public void retraitDe() {
+		
+	}
+	
+	public int getSolde() {
+		
+		return 1;
+	}
+	
+}
