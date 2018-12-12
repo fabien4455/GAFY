2fd43dcd04c805c5d31cb97030429f2eedb83e28
fabien4455 <fabien4455@gmail.com>
2018-12-12 10:50:28 +0100

    Mise à jour du fichier compte

--- TPCompteBancaireJavaSE/src/main/java/fr/epsi/partie11/Compte.java
+++ TPCompteBancaireJavaSE/src/main/java/fr/epsi/partie11/Compte.java
@@ -1,24 +1,24 @@
 package fr.epsi.partie11;
 
 public class Compte {
-	
-	public int solde;
-	
-	public Compte(int pSolde) {
-		this.solde = pSolde;
-	}
-	
-	public void depotDe(int pDepot) {
-		
-	}
-	
-	public void retraitDe(int pRetrait) {
-		
-	}
-	
-	public int getSolde() {
-		
-		return 1;
-	}
-	
+
+    public int solde;
+
+    public Compte( int pSolde ) {
+        this.solde = pSolde;
+    }
+
+    public void depotDe( int pDepot ) {
+        this.solde += pDepot;
+    }
+
+    public void retraitDe( int pRetrait ) {
+        this.solde -= pRetrait;
+    }
+
+    public int getSolde() {
+
+        return this.solde;
+    }
+
 }
--- TPCompteBancaireJavaSE/src/main/java/fr/epsi/partie11/TestCompte.java
+++ TPCompteBancaireJavaSE/src/main/java/fr/epsi/partie11/TestCompte.java
@@ -2,24 +2,24 @@ package fr.epsi.partie11;
 
 public class TestCompte {
 
-	public static void main(String[] args) {
-		Compte compte = new Compte(1000);
-		
-		compte.depotDe(50);
-		System.out.println("Le compte a été crédité de 50 €, le solde est de : " + compte.getSolde() );		
-		compte.retraitDe(25);
-		System.out.println("Le compte a été débité de 25 €, le solde est de : " + compte.getSolde() );
-		
-		compte.depotDe(100);
-		System.out.println("Le compte a été crédité de 100 €, le solde est de : " + compte.getSolde() );
-		compte.retraitDe(50);
-		System.out.println("Le compte a été débité de 50 €, le solde est de : " + compte.getSolde() );
-		
-		compte.depotDe(1000);
-		System.out.println("Le compte a été crédité de 1000 €, le solde est de : " + compte.getSolde() );
-		compte.retraitDe(2000);
-		System.out.println("Le compte a été débité de 2000 €, le solde est de : " + compte.getSolde() );
-		
-	}
+    public static void main( String[] args ) {
+        Compte compte = new Compte( 1000 );
+
+        compte.depotDe( 50 );
+        System.out.println( "Le compte a été crédité de 50 €, le solde est de : " + compte.getSolde() );
+        compte.retraitDe( 25 );
+        System.out.println( "Le compte a été débité de 25 €, le solde est de : " + compte.getSolde() );
+
+        compte.depotDe( 100 );
+        System.out.println( "Le compte a été crédité de 100 €, le solde est de : " + compte.getSolde() );
+        compte.retraitDe( 50 );
+        System.out.println( "Le compte a été débité de 50 €, le solde est de : " + compte.getSolde() );
+
+        compte.depotDe( 1000 );
+        System.out.println( "Le compte a été crédité de 1000 €, le solde est de : " + compte.getSolde() );
+        compte.retraitDe( 2000 );
+        System.out.println( "Le compte a été débité de 2000 €, le solde est de : " + compte.getSolde() );
+
+    }
 
 }
