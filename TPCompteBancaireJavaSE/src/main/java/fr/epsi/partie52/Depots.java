768b41a97b9f5490b9794db5e8fdd82dcbf628a8
VGreg62 <43751188+VGreg62@users.noreply.github.com>
2018-12-15 16:39:16 +0100

    Ajout des classes Compte, TestCompte, Mouvement, Depots, Retrait pour la partie52

---
+++ TPCompteBancaireJavaSE/src/main/java/fr/epsi/partie52/Depots.java
@@ -0,0 +1,23 @@
+package fr.epsi.partie52;
+
+import java.time.LocalDate;
+
+public class Depots extends Mouvement {
+
+	public Depots(int pMontant, LocalDate pDateMvt) {
+		super(pMontant, pDateMvt);
+	}
+	
+	public int getPlus() {
+		return getMontant();
+	}
+
+	public int getMoins() {
+		return 0;
+	}
+
+	public String toString() {
+		return "Vous avez effectué un dépot de " + getMontant() + " euros, le " + getDateMvt();
+	}
+	
+}
